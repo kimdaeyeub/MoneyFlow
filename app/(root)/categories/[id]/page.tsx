272e78344aa1ba5969e357ccbff6e563bef16386
kimdaeyeub <kdy3915@naver.com>
2024-12-07 21:22:24 +0900

    우측 네비게이션바와 해당 컴포넌트에 들어갈 컴포넌트 제작

---
+++ app/(root)/categories/[id]/page.tsx
@@ -0,0 +1,13 @@
+/*
+작성자: 김대엽
+파일의 역할: 카테고리별로 가계부를 필터링해서 보여주는 페이지.
+생성 일자: 2024-12-07
+ */
+
+import React from "react";
+
+const CategoryDetail = () => {
+  return <div>CategoryDetail</div>;
+};
+
+export default CategoryDetail;
--- components/navigation/RightSideBar.tsx
+++ components/navigation/RightSideBar.tsx
@@ -1,10 +1,25 @@
+/*
+작성자: 김대엽
+파일의 역할: 루트레이아웃에서 카테고리별 네비게이션기능을 지원하는 화면 우측 사이드 바이다.
+생성 일자: 2024-12-06
+수정 일자: 2024-12-07
+ */
+
 import React from "react";
+import CategoryCard from "../CategoryCard";
+
+const tags = ["교육", "취미", "음식", "생필품", "도서", "교통비"];
 
 const RightSideBar = () => {
   return (
-    <div className="sticky right-0 top-0 h-screen border-l shadow-sm pt-36 lg:w-[266px]">
-      RightSideBar
-    </div>
+    <nav className="sticky right-0 top-0 h-screen border-l shadow-sm pt-32 px-7 lg:w-[266px]">
+      <h1 className="font-bold text-xl">Category</h1>
+      <ul className="flex flex-col justify-start items-start gap-5 mt-7">
+        {tags.map((tag, index) => (
+          <CategoryCard key={tag} tag={tag} count={index * 2 + 1} />
+        ))}
+      </ul>
+    </nav>
   );
 };
 
