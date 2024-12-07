908bd8c1966d5d132b3645bcb2e88557576aa211
kimdaeyeub <kdy3915@naver.com>
2024-12-07 23:48:16 +0900

    홈화면에 들어갈 원형 그래프 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,24 @@
+/*
+작성자: 김대엽
+파일의 역할: 웹사이트 홈페이지
+생성 일자: 2024-12-06
+수정 일자: 2024-12-07
+ */
+
+import CircleProgressChart from "@/components/charts/CircleProgressChart";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Card>
+        <CardHeader></CardHeader>
+        <CardContent className="flex h-fit">
+          <CircleProgressChart goal={10000} value={7000} text="Daily" />
+          <CircleProgressChart goal={20000} value={10000} text="Weekly" />
+          <CircleProgressChart goal={80000} value={60000} text="Monthly" />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
---
+++ components/charts/CircleProgressChart.tsx
@@ -0,0 +1,96 @@
+/*
+작성자: 김대엽
+파일의 역할: 홈화면에 들어갈 원형 progress graph
+생성 일자: 2024-12-07
+ */
+
+"use client";
+
+import {
+  Label,
+  PolarGrid,
+  PolarRadiusAxis,
+  RadialBar,
+  RadialBarChart,
+} from "recharts";
+
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+const chartData = [
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+];
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  safari: {
+    label: "Safari",
+    color: "#ff6f00b6",
+  },
+} satisfies ChartConfig;
+
+const CircleProgressChart = ({
+  goal,
+  value,
+  text,
+}: {
+  goal: number;
+  value: number;
+  text: string;
+}) => {
+  return (
+    <ChartContainer
+      config={chartConfig}
+      className="mx-auto aspect-square max-h-[250px]"
+    >
+      <RadialBarChart
+        data={chartData}
+        startAngle={0}
+        endAngle={360 * (value / goal)}
+        innerRadius={80}
+        outerRadius={110}
+      >
+        <PolarGrid
+          gridType="circle"
+          radialLines={false}
+          stroke="none"
+          polarRadius={[86, 74]}
+        />
+        <RadialBar dataKey="visitors" background cornerRadius={10} />
+        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                return (
+                  <text
+                    x={viewBox.cx}
+                    y={viewBox.cy}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                  >
+                    <tspan
+                      x={viewBox.cx}
+                      y={viewBox.cy}
+                      className="fill-foreground text-4xl font-bold"
+                    >
+                      {value}
+                    </tspan>
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) + 24}
+                      className="fill-muted-foreground"
+                    >
+                      {text}
+                    </tspan>
+                  </text>
+                );
+              }
+            }}
+          />
+        </PolarRadiusAxis>
+      </RadialBarChart>
+    </ChartContainer>
+  );
+};
+
+export default CircleProgressChart;
