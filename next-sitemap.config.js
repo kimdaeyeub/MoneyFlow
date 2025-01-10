c699cef5d9f4718fa2b59fadffe7790cb2cbbf95
kimdaeyeub <kdy3915@naver.com>
2025-01-10 23:24:37 +0900

    next-sitemap 및 robots.txt 구성 업데이트: 공개 및 비공개 경로에 대한 허용 및 거부 규칙 추가

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,21 +1,60 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://getmoneyflow.vercel.app",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: "daily",
   priority: 0.7,
+  exclude: ["/sign-in", "/create-account", "/github/*"],
   transform: async (config, path) => {
-    if (path === "/") {
+    const publicPaths = [
+      "/sign-in",
+      "/create-account",
+      "/github/start",
+      "/github/complete",
+    ];
+
+    const privatePaths = [
+      "/",
+      "/today",
+      "/graph",
+      "/this-week",
+      "/categories/[id]",
+    ];
+
+    if (publicPaths.includes(path)) {
       return {
         loc: path,
         priority: 1.0,
         changefreq: "daily",
       };
     }
-    return {
-      loc: path,
-      priority: config.priority,
-      changefreq: config.changefreq,
-    };
+
+    if (privatePaths.includes(path)) {
+      return {
+        loc: path,
+        priority: config.priority,
+        changefreq: config.changefreq,
+      };
+    }
+
+    return null;
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: [
+          "/sign-in",
+          "/create-account",
+          "/github/start",
+          "/github/complete",
+        ],
+      },
+      {
+        userAgent: "*",
+        disallow: ["/", "/today", "/graph", "/this-week", "/categories"],
+      },
+    ],
   },
 };
